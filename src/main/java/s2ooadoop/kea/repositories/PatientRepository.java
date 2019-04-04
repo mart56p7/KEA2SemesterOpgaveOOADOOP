0b615ce790f5293270bda489b0535d1136d5e8da
RasmusLCh <RasmusLCh@users.noreply.github.com>
2019-04-04 15:49:13 +0200

    Medicine Update No SQL

--- src/main/java/s2ooadoop/kea/repositories/PatientRepository.java
+++ src/main/java/s2ooadoop/kea/repositories/PatientRepository.java
@@ -2,6 +2,36 @@ package s2ooadoop.kea.repositories;
 
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.util.Date;
+
 @Repository
-public class PatientRepository {
+public class PatientRepository implements PatientRepositoryInterface {
+
+    private Database Database;
+
+    @Override
+    public ResultSet GetPatient(int ID) {
+        return null;
+    }
+
+    @Override
+    public ResultSet GetPatients(String SortBy, boolean asc) {
+        return null;
+    }
+
+    @Override
+    public int CreatePatient(int CPR, Date Birthday, String Firstname, String Lastname, String Phonenumber, String Address, float Height, float Weight, String Description) {
+        return 0;
+    }
+
+    @Override
+    public void EditPatient(int CPR, Date Birthday, String Firstname, String Lastname, String Phonenumber, String Address, float Height, float Weight, String Description, int ID) {
+
+    }
+
+    @Override
+    public void DeletePatient(int ID) {
+
+    }
 }
--- src/main/java/s2ooadoop/kea/repositories/PatientRepositoryInterface.java
+++ src/main/java/s2ooadoop/kea/repositories/PatientRepositoryInterface.java
@@ -2,6 +2,23 @@ package s2ooadoop.kea.repositories;
 
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.util.Date;
+
 @Repository
-public class PatientRepositoryInterface {
+public interface PatientRepositoryInterface {
+
+
+    ResultSet GetPatient(int ID);
+
+    ResultSet GetPatients(String SortBy, boolean asc);
+
+
+    int CreatePatient(int CPR, Date Birthday, String Firstname, String Lastname, String Phonenumber, String Address, float Height, float Weight, String Description);
+
+    void EditPatient(int CPR, Date Birthday, String Firstname, String Lastname, String Phonenumber, String Address, float Height, float Weight, String Description, int ID);
+
+  
+    void DeletePatient(int ID);
+
 }
